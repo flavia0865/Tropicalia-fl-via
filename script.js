75cbc89b9dd0b12f14441da5fb032fad34e2c57e
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2025-04-22 14:02:42 -0300

    22/04

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', function () {
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
     });
 
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute ('aria-label') === 'true';
+    botaoDeAcessibilidade.setAttribute ('aria-expanded', !botaoSelecionado);
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
